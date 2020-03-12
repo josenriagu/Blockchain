bea93afd1ff2faa271bf5d6b260dc0178edc692c
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-03-12 15:47:31 +0100

    feat(python): implement blockchain methods

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -32,12 +32,19 @@ class Blockchain(object):
 
         block = {
             # TODO
+            "index": len(self.chain) + 1,
+            "timestamp": time(),
+            "transactions": self.current_transactions,
+            "proof": proof,
+            "previous_hash": previous_hash or self.hash(self.chain[-1])
         }
 
         # Reset the current list of transactions
-        # Append the chain to the block
+        self.current_transactions = []
+        # Append the block to the chain
+        self.chain.append(block)
         # Return the new block
-        pass
+        return block
 
     def hash(self, block):
         """
@@ -56,9 +63,9 @@ class Blockchain(object):
         # or we'll have inconsistent hashes
 
         # TODO: Create the block_string
-
+        block_string = json.dumps(block, sort_keys=True).encode()
         # TODO: Hash this string using sha256
-
+        hashed_block_string = hashlib.sha256(block_string).hexdigest()
         # By itself, the sha256 function returns the hash in a raw string
         # that will likely include escaped characters.
         # This can be hard to read, but .hexdigest() converts the
@@ -66,7 +73,7 @@ class Blockchain(object):
         # easier to work with and understand
 
         # TODO: Return the hashed block string in hexadecimal format
-        pass
+        return hashed_block_string
 
     @property
     def last_block(self):
@@ -97,8 +104,12 @@ class Blockchain(object):
         :return: True if the resulting hash is a valid proof, False otherwise
         """
         # TODO
-        pass
-        # return True or False
+        # set a initial guess: concatenate block string and proof then encode them
+        guess = f"{block_string}{proof}".encode()
+        # create a guess hash and hexdigest it
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        # then return True if the guess hash has the valid number of leading zeros otherwise return False
+        return guess_hash[:6] == "000000"
 
 
 # Instantiate our Node
@@ -111,27 +122,77 @@ node_identifier = str(uuid4()).replace('-', '')
 blockchain = Blockchain()
 
 
-@app.route('/mine', methods=['GET'])
-def mine():
-    # Run the proof of work algorithm to get the next proof
-
-    # Forge the new Block by adding it to the chain with the proof
-
-    response = {
-        # TODO: Send a JSON response with the new block
-    }
-
+@app.route('/', methods=['GET'])
+def home():
+    response = "Hey you, welcome to the UltimateMiner"
     return jsonify(response), 200
+
+
+@app.route('/mine', methods=['POST'])
+def mine():
+    '''
+    Run the proof of work algorithm to get the next proof
+    Forge the new Block by adding it to the chain with the proof
+    '''
+
+    # handle non json response
+    data = request.get_json()
+
+    # require the proof and id to be present
+    required = ['proof', 'id']
+
+    # if the values from data are not in required
+    if not all(k in data for k in required):
+        # then send a json message of missing values
+        response = {'message': "Missing Values"}
+        # return a 400 error
+        return jsonify(response), 400
+
+    # get the submitted proof from data
+    submitted_proof = data.get('proof')
+
+    # determine if proof is valid
+    last_block = blockchain.last_block
+    last_block_string = json.dumps(last_block, sort_keys=True)
+    if blockchain.valid_proof(last_block_string, submitted_proof):
+        # forge the new block
+        previous_hash = blockchain.hash(last_block)
+        block = blockchain.new_block(submitted_proof, previous_hash)
+        # build a response dictionary
+
+        response = {
+            'message': "New Block Forged",
+            'index': block['index'],
+            'transactions': block['transactions'],
+            'proof': block['proof'],
+            'previous_hash': block['previous_hash']
+        }
+        # return the response
+        return jsonify(response), 200
+    # otherwise
+    else:
+        # send a json mesage that the proof was invalid
+        response = {'message': "Proof was invalid or already submitted"}
+
+        return jsonify(response), 200
 
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
     response = {
         # TODO: Return the chain and its current length
+        "length": len(blockchain.chain),
+        "chain": blockchain.chain
     }
+    return jsonify(response), 200
+
+
+@app.route('/last_block', methods=['GET'])
+def last_block():
+    response = {'last_block': blockchain.last_block}
     return jsonify(response), 200
 
 
 # Run the program on port 5000
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='localhost', port=5000)
